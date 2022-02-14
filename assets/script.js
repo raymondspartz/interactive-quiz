9a8df6748941130cacdd229e2d27b8125d6b6cab
Raymond Spartz <raymond.luke.spartz@gmail.com>
2022-02-14 11:23:54 -0600

    added folders

--- assets/script.js
+++ assets/script.js
@@ -96,7 +96,6 @@ let newCheckAnswerSection = function () {
 
 ////////////////////////////////////////////////////////////////////////////
 
-//
 let loadQuestion = function () {
   let questionKey = "question" + currentView;
   let currentQuestion = questionObj[questionKey].title;
@@ -105,3 +104,32 @@ let loadQuestion = function () {
 };
 
 document.querySelector(".quizBegin").addEventListener("click", quizBegin);
+
+let loadAnswers = function () {
+  let answerButtons = document.querySelector(".answerDiv").children;
+  let arr = [];
+  let num;
+  for (let i = 0; i < answerButtons.length; i++) {
+    let randomize = function () {
+      // Give a random value to num that is between 1 and the length of
+      // the amount of children, or answer choices, for each slide.
+      num = Math.floor(Math.random() * answerButtons.length) + 1;
+      // Check each random number; If that number has already been chosen,
+      // run the randomize function again to find a unique one.
+      if (arr.indexOf(num) >= 0) {
+        return randomize();
+      }
+      return num;
+    };
+    // Add the unique randomized num to the array.
+    arr.push(randomize());
+  }
+  for (let i = 0; i < answerButtons.length; i++) {
+    answerButtons[i].id = "answer" + arr[i];
+    // Determine which answer values to grab and display based on the
+    // indivdual buttons assigned id.
+    let questionKey = "question" + currentSlide;
+    let val = arr[i] - 1;
+    answerButtons[i].textContent = questionObj[questionKey].answers[val];
+  }
+};
